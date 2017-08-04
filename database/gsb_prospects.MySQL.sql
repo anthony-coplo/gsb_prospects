c8a2a516ffcae22da03faffc835732d0ed385d09
David RIEHL <david.riehl@ac-lille.fr>
2017-08-04 10:24:59 +0200

    initial commit

---
+++ database/gsb_prospects.MySQL.sql
@@ -0,0 +1,229 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.4
+-- https://www.phpmyadmin.net/
+--
+-- Client :  127.0.0.1
+-- Généré le :  Jeu 03 Août 2017 à 10:44
+-- Version du serveur :  5.7.14
+-- Version de PHP :  7.0.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `gsb_prospects`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `client`
+--
+
+CREATE TABLE `client` (
+  `id_Praticien` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `etat`
+--
+
+CREATE TABLE `etat` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `interesser`
+--
+
+CREATE TABLE `interesser` (
+  `id_Prestation` int(11) NOT NULL,
+  `id_Client` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `praticien`
+--
+
+CREATE TABLE `praticien` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(25) NOT NULL,
+  `prenom` varchar(25) NOT NULL,
+  `adresse` varchar(255) NOT NULL,
+  `id_Ville` int(11) NOT NULL,
+  `code_Type_Praticien` varchar(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `prestation`
+--
+
+CREATE TABLE `prestation` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `prospect`
+--
+
+CREATE TABLE `prospect` (
+  `id_Praticien` int(11) NOT NULL,
+  `id_Etat` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `type_praticien`
+--
+
+CREATE TABLE `type_praticien` (
+  `code` varchar(6) NOT NULL,
+  `libelle` varchar(50) NOT NULL,
+  `lieu` varchar(70) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `ville`
+--
+
+CREATE TABLE `ville` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(45) NOT NULL,
+  `code_postal` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Index pour les tables exportées
+--
+
+--
+-- Index pour la table `client`
+--
+ALTER TABLE `client`
+  ADD PRIMARY KEY (`id_Praticien`);
+
+--
+-- Index pour la table `etat`
+--
+ALTER TABLE `etat`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `interesser`
+--
+ALTER TABLE `interesser`
+  ADD PRIMARY KEY (`id_Prestation`,`id_Client`),
+  ADD KEY `id_Client` (`id_Client`);
+
+--
+-- Index pour la table `praticien`
+--
+ALTER TABLE `praticien`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_Praticien_id_Ville` (`id_Ville`),
+  ADD KEY `FK_Praticien_code_Type_Praticien` (`code_Type_Praticien`);
+
+--
+-- Index pour la table `prestation`
+--
+ALTER TABLE `prestation`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `prospect`
+--
+ALTER TABLE `prospect`
+  ADD PRIMARY KEY (`id_Praticien`),
+  ADD KEY `FK_Prospect_id_Etat` (`id_Etat`);
+
+--
+-- Index pour la table `type_praticien`
+--
+ALTER TABLE `type_praticien`
+  ADD PRIMARY KEY (`code`);
+
+--
+-- Index pour la table `ville`
+--
+ALTER TABLE `ville`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
+
+--
+-- AUTO_INCREMENT pour la table `etat`
+--
+ALTER TABLE `etat`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `praticien`
+--
+ALTER TABLE `praticien`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `prestation`
+--
+ALTER TABLE `prestation`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `ville`
+--
+ALTER TABLE `ville`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Contraintes pour les tables exportées
+--
+
+--
+-- Contraintes pour la table `client`
+--
+ALTER TABLE `client`
+  ADD CONSTRAINT `FK_Client_id_Praticien` FOREIGN KEY (`id_Praticien`) REFERENCES `praticien` (`id`);
+
+--
+-- Contraintes pour la table `interesser`
+--
+ALTER TABLE `interesser`
+  ADD CONSTRAINT `FK_interesser_id_Prestation` FOREIGN KEY (`id_Prestation`) REFERENCES `prestation` (`id`),
+  ADD CONSTRAINT `interesser_ibfk_1` FOREIGN KEY (`id_Client`) REFERENCES `client` (`id_Praticien`);
+
+--
+-- Contraintes pour la table `praticien`
+--
+ALTER TABLE `praticien`
+  ADD CONSTRAINT `FK_Praticien_code_Type_Praticien` FOREIGN KEY (`code_Type_Praticien`) REFERENCES `type_praticien` (`code`),
+  ADD CONSTRAINT `FK_Praticien_id_Ville` FOREIGN KEY (`id_Ville`) REFERENCES `ville` (`id`);
+
+--
+-- Contraintes pour la table `prospect`
+--
+ALTER TABLE `prospect`
+  ADD CONSTRAINT `FK_Prospect_id_Etat` FOREIGN KEY (`id_Etat`) REFERENCES `etat` (`id`),
+  ADD CONSTRAINT `FK_Prospect_id_Praticien` FOREIGN KEY (`id_Praticien`) REFERENCES `praticien` (`id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
