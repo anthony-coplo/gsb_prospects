1745058491d9c08a2cdc36264bae17cdd2378c9c
David RIEHL <david.riehl@ac-lille.fr>
2017-08-04 10:57:45 +0200

    add kernel object NotImplementedException

---
+++ src/kernel/NotImplementedException.php
@@ -0,0 +1,15 @@
+<?php
+namespace gsb_prospects\kernel;
+
+use \BadMethodCallException;
+
+class NotImplementedException extends BadMethodCallException {
+
+    public function __construct($message = "", $code = 0, $previous = null) {
+        parent::__construct($message, $code, $previous);
+        if(empty($message)) {
+            $this->message = "This function is not yet implemented.";
+        }
+    }
+}
+?>
