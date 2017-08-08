8cf49360c1c6c9083d4dfb749c6caa360baa9891
David RIEHL <david.riehl@ac-lille.fr>
2017-08-08 10:21:10 +0200

    update NotImplementedException

--- src/kernel/NotImplementedException.php
+++ src/kernel/NotImplementedException.php
@@ -1,9 +1,7 @@
 <?php
 namespace gsb_prospects\kernel;
 
-use \BadMethodCallException;
-
-final class NotImplementedException extends BadMethodCallException {
+final class NotImplementedException extends AbstractException {
 
     public function __construct($message = "", $code = 0, $previous = null) {
         parent::__construct($message, $code, $previous);
