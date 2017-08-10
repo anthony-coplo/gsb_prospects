3a1697db4f2a42d731458ada5f23833fc8932f32
David RIEHL <david.riehl@ac-lille.fr>
2017-08-10 15:51:47 +0200

    update TypePraticien

--- src/model/dao/TypePraticienDAO.php
+++ src/model/dao/TypePraticienDAO.php
@@ -7,63 +7,12 @@ use gsb_prospects\model\objects\TypePraticien;
 
 final class TypePraticienDAO extends AbstractDAO implements IDAO {
     protected $table = "type_praticien";
+    protected $class = "gsb_prospects\model\objects\TypePraticien";
+    protected $fields = array("id", "code", "libelle", "lieu");
 
     public function delete($object)
     {
         throw new NotImplementedException();
-    }
-
-    public function find($id)
-    {
-        $dbh = $this->getConnexion();
-
-        $query = "SELECT * FROM `{$this->table}`
-                  WHERE `id` = :id;";
-        
-        $sth = $dbh->prepare($query);
-        $sth->bindParam(":id", $id);
-        $sth->setFetchMode(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE, "gsb_prospects\model\objects\TypePraticien", array("id", "code", "libelle", "lieu"));
-        $sth->execute();
-
-        $object = $sth->fetch();
-
-        $this->closeConnexion();
-
-        if(! $object){
-            $message = $sth->errorInfo()[2];    // Error Message
-            $code = $sth->errorInfo()[0];       // SQLSTATE
-            if($code == 0 && empty($message)) {
-                $message = "unknown id $id";
-            }
-            throw new DAOException($message, $code);
-        }
-
-        return $object;
-    }
-
-    public function findAll()
-    {
-        $dbh = $this->getConnexion();
-
-        $query = "SELECT * FROM `{$this->table}`;";
-        
-        $sth = $dbh->prepare($query);
-        $sth->setFetchMode(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE, "gsb_prospects\model\objects\TypePraticien", array("id", "code", "libelle", "lieu"));
-        $sth->execute();
-
-        $objects = $sth->fetchAll();
-
-        $this->closeConnexion();
-
-        if(! $objects){
-            $message = $sth->errorInfo()[2];    // Error Message
-            $code = $sth->errorInfo()[0];       // SQLSTATE
-            if ($code != 0){
-                throw new DAOException($message, $code);
-            }
-        }
-
-        return $objects;
     }
 
     public function insert($object)
