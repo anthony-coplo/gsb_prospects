71c80ca41c31e041a715765efc6a57cb70b6fcac
David RIEHL <david.riehl@ac-lille.fr>
2017-08-21 16:35:05 +0200

    update index with 1st level route definition

--- src/index.php
+++ src/index.php
@@ -9,6 +9,10 @@
  */
 namespace gsb_prospects;
 
+use gsb_prospects\kernel\Route;
+use gsb_prospects\kernel\Router;
+use gsb_prospects\view\View;
+
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/parameters.php';
 
@@ -16,11 +20,19 @@ use gsb_prospects\controller\ClientController;
 use gsb_prospects\controller\PraticienController;
 use gsb_prospects\controller\ProspectController;
 
-//$praticienController = new PraticienController();
-//$praticienController->defaultAction();
-
-$prospectController = new ProspectController();
-$prospectController->defaultAction();
-
-//$clientController = new ClientController();
-//$clientController->defaultAction();
+// 1st level route definition
+$router = new Router();
+$router->addRoute(new Route("/", "DefaultController"));
+$router->addRoute(new Route("/accueil", "DefaultController"));
+$router->addRoute(new Route("/praticiens", "PraticienController"));
+$router->addRoute(new Route("/praticien/{slug}", "PraticienController"));
+$router->addRoute(new Route("/prospects", "ProspectController"));
+$router->addRoute(new Route("/prospect/{slug}", "ProspectController"));
+$router->addRoute(new Route("/clients", "ClientController"));
+$router->addRoute(new Route("/client/{slug}", "ClientController"));
+$route = $router->findRoute();
+if ($route) {
+    $route->execute();
+} else {
+    print("<p>Page inconnue.</p>" . PHP_EOL);
+}
