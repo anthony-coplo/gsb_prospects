273bc44d5fcbf1ba8b795d0a27c88657e15a7cab
David RIEHL <david.riehl@ac-lille.fr>
2017-08-21 12:57:49 +0200

    update PraticienController and template

--- src/controller/PraticienController.php
+++ src/controller/PraticienController.php
@@ -11,10 +11,6 @@ namespace gsb_prospects\controller;
 
 use gsb_prospects\model\dao\PraticienDAO;
 use gsb_prospects\model\objects\Praticien;
-use gsb_prospects\model\dao\TypePraticienDAO;
-use gsb_prospects\model\objects\TypePraticien;
-use gsb_prospects\model\dao\VilleDAO;
-use gsb_prospects\model\objects\Ville;
 use gsb_prospects\view\View;
 
 /**
@@ -43,20 +39,13 @@ final class PraticienController
     public function listAction()
     {
         $praticienDAO = new PraticienDAO();
-        $typePraticienDAO = new TypePraticienDAO();
-        $villeDAO = new VilleDAO();
 
         $objects = $praticienDAO->findAll();
-        foreach ($objects as $object) {
-            $id = $object->getId();
-            $ville = $villeDAO->findFromPraticien($id);
-            $object->setVille($ville);
-            $typePraticien = $typePraticienDAO->findFromPraticien($id);
-            $object->setTypePraticien($typePraticien);
-        }
 
         $view = new View("Praticien_List");
         $view->bind("title", "Liste des Praticiens");
+        $view->bind("objectName", "praticien");
+        $view->bind("objectNamePlural", "praticiens");
         $view->bind("objects", $objects);
         $view->display();
     }
