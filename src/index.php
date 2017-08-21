3714ed7c1200bfe4a9d826a90b19f8d332e3cc85
David RIEHL <david.riehl@ac-lille.fr>
2017-08-21 13:00:12 +0200

    update index test templates

--- src/index.php
+++ src/index.php
@@ -12,10 +12,15 @@ namespace gsb_prospects;
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/parameters.php';
 
-//include "view/index.html";
-
+use gsb_prospects\controller\ClientController;
 use gsb_prospects\controller\PraticienController;
-use gsb_prospects\Project;
+use gsb_prospects\controller\ProspectController;
 
-$praticienController = new PraticienController();
-$praticienController->defaultAction();
+//$praticienController = new PraticienController();
+//$praticienController->defaultAction();
+
+$prospectController = new ProspectController();
+$prospectController->defaultAction();
+
+//$clientController = new ClientController();
+//$clientController->defaultAction();
